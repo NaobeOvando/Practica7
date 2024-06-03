c2e9ce963c12ff9a10edd3e471165acaf3d526a9
HP <HP@DESKTOP-3QTPP5V>
2024-06-03 14:38:33 -0500

    Agregar archivos de proyecto.

---
+++ Middleware/Logica/MiddlewareProductos.cs
@@ -0,0 +1,49 @@
+﻿using Datos.DTO;
+using Datos.Entidades;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logica
+{
+    public class MiddlewareProductos
+    {
+        servicioTienda.WebServiceVentas webTiendaMil = new servicioTienda.WebServiceVentas();
+
+        public List<servicioTienda.Producto> ListaTiendaMil()
+        {
+            return webTiendaMil.ListarProductos().ToList();
+        }
+
+        public List<DTOProducto> ListaProductosSOAP()
+        {
+            List<DTOProducto> lista = new List<DTOProducto>();
+            foreach (var item in ListaTiendaMil())
+            {
+                DTOProducto datoProducto = new DTOProducto();
+                datoProducto.Id = item.idProducto;
+                datoProducto.Name = item.nombre;
+                datoProducto.Price = (double) item.precio_unitario;
+                if(item.iva == 1)
+                    datoProducto.Iva = true;
+                else datoProducto.Iva = false;
+                lista.Add(datoProducto);
+            }
+
+            return lista;
+        }
+        public List<ProductosSuperX> listarProductosSuperX()
+        {
+            string url = "http://localhost:50070/api/Productos";
+            WebClient respuesta = new WebClient();
+            string get = respuesta.DownloadString(new Uri(url));
+            List<ProductosSuperX> listaSuperX = JsonConvert.DeserializeObject<List<ProductosSuperX>>(get);
+            return listaSuperX;
+        }
+
+    }
+}
